f191f719af5859c3eedcc98c48f011d8a8cd42df
okokabcd@learn <1013081+okokabcd@user.noreply.gitee.com>
2023-11-24 16:56:10 +0800

    UPDATE index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import createComp from './js/comp';
 import '@/js/resources';
 import { add } from './js/math';
 
-const texts: Array<string> = ['hello', 'CICD + 代码规范方案 123'];
+const texts: Array<string> = ['hello', 'CICD + 代码规范方案 1234'];
 // const comp = createComp('h2', { class: 'content' }, texts.at(-1));
 const comp = createComp('h2', { class: 'content' }, texts.join(' '));
 document.body.appendChild(comp);
